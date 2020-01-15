a420cf24647d83011173ff3cff2d989c9cd92ccf
hootan1 <56854354+hootan1@users.noreply.github.com>
2020-01-15 13:14:39 -0800

    Navbar Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 
-import { pages } from './pages';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pages = pages;
   title = 'Musō Marketing 無双™';
 }
