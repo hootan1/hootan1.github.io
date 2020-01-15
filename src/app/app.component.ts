af49117e1436aab2cf7136f3f9644f8bf606095d
hootan1 <56854354+hootan1@users.noreply.github.com>
2020-01-15 10:39:53 -0800

    Converted Navbar to Angular Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { pages } from './pages';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hootan1';
+  pages = pages;
+  title = 'Musō Marketing 無双™';
 }
